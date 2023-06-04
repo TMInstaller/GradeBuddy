3c3e6e0b20d98e6e59c70f10e1475dab49a53f98
TMInstaller <ajvldmlqjqclr@naver.com>
2023-06-04 21:25:46 +0900

    JSP 파일 사전 생성, 페이지 설명 주석 추가

--- src/main/java/ac/tukorea/gradebuddy/MainPageController.java
+++ src/main/java/ac/tukorea/gradebuddy/MainPageController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainPageController {
     @GetMapping("/") // 1. 슬래시(/) 뒤에 있는 URL에 있다면
     public String hello() {
-        return "main_page"; // 2. main_page.jsp 파일을 반환합니다.
+        return "main_page"; // 2. main_page.jsp 파일을 반환합니다. views 폴더에 있는 파일을 반환합니다.
     }
 }

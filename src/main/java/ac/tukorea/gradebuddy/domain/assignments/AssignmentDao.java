3347b2f0709ea25e03f3ceebb7224debbea96d64
TMInstaller <ajvldmlqjqclr@naver.com>
2023-06-05 02:06:26 +0900

    Mapper, Dao 연결하기

--- src/main/java/ac/tukorea/gradebuddy/domain/assignments/AssignmentDao.java
+++ src/main/java/ac/tukorea/gradebuddy/domain/assignments/AssignmentDao.java
@@ -1,5 +1,23 @@
 // DAO: 데이터베이스 접근을 담당하며, 데이터 조회, 삽입, 업데이트, 삭제 등의 SQL 쿼리를 실행합니다.
 package ac.tukorea.gradebuddy.domain.assignments;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public class AssignmentDao {
+
+    private final SqlSession sqlSession;
+
+    @Autowired
+    public AssignmentDao(SqlSession sqlSession) {
+        this.sqlSession = sqlSession;
+    }
+
+    public List<Assignment> getAllAssignments() {
+        return sqlSession.selectList("getAllAssignments");
+    }
 }
--- src/main/java/ac/tukorea/gradebuddy/domain/feedbacks/FeedbackDao.java
+++ src/main/java/ac/tukorea/gradebuddy/domain/feedbacks/FeedbackDao.java
@@ -1,5 +1,23 @@
 // DAO: 데이터베이스 접근을 담당하며, 데이터 조회, 삽입, 업데이트, 삭제 등의 SQL 쿼리를 실행합니다.
 package ac.tukorea.gradebuddy.domain.feedbacks;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public class FeedbackDao {
+
+    private final SqlSession sqlSession;
+
+    @Autowired
+    public FeedbackDao(SqlSession sqlSession) {
+        this.sqlSession = sqlSession;
+    }
+
+    public List<Feedback> getAllFeedbacks() {
+        return sqlSession.selectList("getAllFeedbacks");
+    }
 }
